0598a6c4c9a7aeaac6aac81e87192436a6a11b0c
Sam007k <msameer007k@gmail.com>
2021-09-15 22:10:21 +0530

    first commit

---
+++ index.js
@@ -0,0 +1,13 @@
+const menu = document.getElementById('menu');
+
+
+const menubtn = document.getElementById('menu-btn');
+console.log('he');
+
+
+menubtn.addEventListener('click', () =>{
+
+    menu.classList.toggle('show-menu');
+   
+
+} );
